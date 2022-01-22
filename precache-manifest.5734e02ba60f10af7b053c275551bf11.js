509fb1bf1226e8f97b096c18d4035e3a3c4c4a1a
heyynat <natalifplima@gmail.com>
2022-01-22 18:26:51 -0400

    Updates

---
+++ precache-manifest.5734e02ba60f10af7b053c275551bf11.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "583af6c02e7f00a24df767bc93a82145",
+    "url": "/trivia/index.html"
+  },
+  {
+    "revision": "39628af3fc33f8a4b96a",
+    "url": "/trivia/static/css/main.a6b8f4d4.chunk.css"
+  },
+  {
+    "revision": "48a5e2ed291576043dc4",
+    "url": "/trivia/static/js/2.e2d527f5.chunk.js"
+  },
+  {
+    "revision": "5d1b8067085602e259fd0862f57df793",
+    "url": "/trivia/static/js/2.e2d527f5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "39628af3fc33f8a4b96a",
+    "url": "/trivia/static/js/main.01152e6a.chunk.js"
+  },
+  {
+    "revision": "23dbb679c736924e4b1a",
+    "url": "/trivia/static/js/runtime-main.6e763e45.js"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/trivia/static/media/trivia.466d153e.png"
+  }
+]);
